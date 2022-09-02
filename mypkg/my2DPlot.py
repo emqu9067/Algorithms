24a14a12f39bf7ca0a926cd568ab2eb803d83fb5
emqu9067 <emqu9067@colorado.edu>
2022-09-01 20:44:30 -0600

    updating comments to lab1

--- mypkg/my2DPlot.py
+++ mypkg/my2DPlot.py
@@ -8,16 +8,21 @@ class my2DPlot:
             y = f(x)
             self.p = plt.plot(x,y)
     def show(self):
+            #this function shows the plot on your screen
             plt.show()
     def dotted(self):
+            #this function changes the last plot to be dotted instead of solid
             self.p[-1].set_linestyle('dotted')
     def labels(self,x,y):
+            #this plot changes the labels on the plot figure
             plt.xlabel(x)
             plt.ylabel(y)
     def addPlot(self,f):
+            #this function adds multiple plots on a single figure
             x=self.p[0].get_data()[0]
             plt.plot(x,f(x))
     def color(self,colorName):
+            #this function changes the color of the last plotted line
             self.p[-1].set_color(colorName)
     def logy(self):
             plt.yscale('log')
