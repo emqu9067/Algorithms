33a7bde5193d9b6ad87544bc3f2a9f5829aa956a
emqu9067 <emqu9067@colorado.edu>
2022-09-29 14:12:26 -0600

    lab6

---
+++ mypkg/lab6.py
@@ -0,0 +1,2 @@
+import math
+def evalF(x):return [4*x[0]**2 + x[1]**2-4,x[0]+x[1]-math.sin(x[0]-x[1])]
--- mypkg/lab6_prelab.py
+++ mypkg/lab6_prelab.py
@@ -6,8 +6,8 @@ print(f(0))
 def fd(s,h):
     j=len(h)
     i=0
-    v1=numpy.zeros((j,1))
-    v2=numpy.zeros((j,1))
+    v1=numpy.zeros(j)
+    v2=numpy.zeros(j)
     while(i<j):
         v1[i]=(f(s+h[i])-f(s))/h[i]
         v2[i]=(f(s+h[i])-f(s-h[i]))/(2*h[i])
@@ -18,7 +18,8 @@ h=(.01*2*-numpy.arange(1,11))
 print(h)
 [a1,a2]=fd((math.pi)/2,h)
 print(math.pi)
-print(numpy.transpose(a1))
+print
+print(a1)
 print(a2)
 
 print(abs(a1[0]+1)/abs(a1[1]+1))
